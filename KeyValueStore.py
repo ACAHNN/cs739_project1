fc5c5463991ee8f556004129b73eadb00f352caf
David Tran-Lam <david@localhost.localdomain>
2014-09-19 15:17:32 -0500

    changed up keyValuestore

--- KeyValueStore.py
+++ KeyValueStore.py
@@ -4,21 +4,21 @@ class KeyValueStore:
 	m_keyValues = {}
 	m_backingStore = 0
 	def __init__(self, fileName):
-		m_backingStore = open(fileName, "w+")		
-		#read from backing store and place into dictionary
-
-	def put(key, value, oldValue):
+		m_backingStore = open(fileName, "ab")		
+		#do pickle stuff
+		
+	def put(key, value):
 		#what is the failure case for this?
-		success = get(key, tmpOldValue)
+		oldValue,success = get(key)
 		m_keyValues[key] = value
 
+	#	m_backingStore.write(str(m_keyValues)) pickle stuff
 
-		#write/update to backing store
-		return success			
-	def get(key, value):
+		return oldValue, success			
+	def get(key):
 		#what is a failure case for this?? spec says to return -1 on failure
 		if m_keyValues.has_key(key):
 			value = m_keyValues[key]
-			return 0
+			return value, 0
 		else:
-			return 1 
+			return None, 1 
