89a87b2d73a65f209b90a6222f287c2365b7c1ee
David Tran-Lam <david@localhost.localdomain>
2014-09-19 14:45:44 -0500

    needs to write to backingstore and read from it, also needs to handle error cases that i can't think of

--- KeyValueStore.py
+++ KeyValueStore.py
@@ -5,6 +5,21 @@ class KeyValueStore:
 	m_backingStore = 0
 	def __init__(self, fileName):
 		m_backingStore = open(fileName, "w+")		
+		#read from backing store and place into dictionary
+
+	def put(key, value, oldValue):
+		success = get(key, tmpOldValue)
+		m_keyValues[key] = value
 
 
+		#write/update to backing store
+		return success			
+	def get(key, value):
+		#what is a failure case for this?? spec says to return -1 on failure
+		if m_keyValues.has_key(key):
+			value = m_keyValues[key]
+			return 0
+		else:
+			return 1 
 x = KeyValueStore("test")
+print "hello world"
