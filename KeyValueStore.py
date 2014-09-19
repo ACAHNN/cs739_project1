7fe1cb6ae69106ee869531e7ff0759a224e3cdeb
David Tran-Lam <david@localhost.localdomain>
2014-09-19 15:25:19 -0500

    fixed up keyValueStore and started server skeleton

--- KeyValueStore.py
+++ KeyValueStore.py
@@ -4,21 +4,21 @@ class KeyValueStore:
 	m_keyValues = {}
 	m_backingStore = 0
 	def __init__(self, fileName):
-		m_backingStore = open(fileName, "ab")		
+		self.m_backingStore = open(fileName, "ab")		
 		#do pickle stuff
 		
-	def put(key, value):
+	def put(self, key, value):
 		#what is the failure case for this?
-		oldValue,success = get(key)
-		m_keyValues[key] = value
+		oldValue,success = self.get(key)
+		self.m_keyValues[key] = value
 
 	#	m_backingStore.write(str(m_keyValues)) pickle stuff
 
 		return oldValue, success			
-	def get(key):
+	def get(self,key):
 		#what is a failure case for this?? spec says to return -1 on failure
-		if m_keyValues.has_key(key):
-			value = m_keyValues[key]
+		if self.m_keyValues.has_key(key):
+			value = self.m_keyValues[key]
 			return value, 0
 		else:
 			return None, 1 
--- Server.py
+++ Server.py
@@ -1,1 +1,7 @@
 #server class.  Will interact with protobuffer to parse messages and to call the correct function in the class KeyValueStore.  Needs to handle failure cases
+
+from KeyValueStore import KeyValueStore as KVS
+class Server:
+	m_kvs = None
+	def __init__(self, fileName):
+		self.m_kvs = KVS(fileName)
