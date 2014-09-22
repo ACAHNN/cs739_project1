182d898ed1efb1d662aff785c606b173653d9003
Aaron Cahn <cahn@optimus.cs.wisc.edu>
2014-09-21 23:06:56 -0500

    implementing the base functionality for KeyValueStore class

--- Client.py
+++ Client.py
@@ -7,11 +7,22 @@ from google.protobuf.text_format import *
 from sys import stdout
 
 if __name__ == "__main__":
+  
+  # open the tcp channel
   channel = TcpChannel(("localhost", 8080))
   proxy = Proxy(KVService_Stub(channel))
+  
+  # test of SetRequest
   request = SetRequest()
   request.key = "Hello"
   request.value = "World"
   response = proxy.KVService.set(request)
+  for r in response:
+    PrintMessage(r, stdout, 0)
+
+  # test of GetRequest
+  request = GetRequest()
+  request.key = "Hello"
+  response = proxy.KVService.get(request)
   for r in response:
     PrintMessage(r, stdout, 0)
--- KeyValueStore.py
+++ KeyValueStore.py
@@ -1,21 +1,60 @@
 #!/usr/bin/python
 
+#TODO: Decide the failure models for KeyValueStore and handle them accordingly
+
 class KeyValueStore:
-	m_keyValues = {}
-	m_backingStore = 0
+	
 	def __init__(self, fileName):
-		self.m_backingStore = open(fileName, "ab")		
-		#do pickle stuff
 		
+		# in memory key value store
+		self.m_keyValues = {}		
+
+		# reference to backing file name so we can
+		# open and close the file as needed
+		self.backingStore = fileName
+		
+		# populate the in memory key value store
+		self._read_backstore()
+
+		
+	def _read_backstore(self):
+		"""
+		Whenever a Server instance comes on line it will
+		initialize an instance of KeyValueStore which will
+		then read in the key value pairs from the persistent
+		store (a file in this case). The file is read from 
+		start to finish which allows it replay key value 
+		updates as a log. An new key value pair will always be
+		read last and therefore overwrite a stale version.
+		"""
+		
+		# open the persistent key value store
+		with open(self.backingStore, "rb") as ifile:
+			for line in ifile:
+				key, value = line.split("||")
+				
+				self.m_keyValues[key] = value
+		
+	
+	def _write_backstore(self, key, value):
+		
+		# write a key value pair to the backstore
+		with open(self.backingStore, "ab") as ifile:
+			ifile.write("%s || %s\n" % (key, value))
+	
 	def put(self, key, value):
+		
 		#what is the failure case for this?
 		oldValue,success = self.get(key)
 		self.m_keyValues[key] = value
 
-	#	m_backingStore.write(str(m_keyValues)) pickle stuff
+		# make the key value update persistent
+		self._write_backstore(key, value)
 
 		return oldValue, success			
+	
 	def get(self,key):
+		
 		#what is a failure case for this?? spec says to return -1 on failure
 		if self.m_keyValues.has_key(key):
 			value = self.m_keyValues[key]
