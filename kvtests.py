1a96e5ca5ae167f982720cbfe7cafff7d309e953
Aaron Cahn <cahn@adelie-03.cs.wisc.edu>
2014-09-25 17:33:05 -0500

    updated kvtests.py

--- kvtests.py
+++ kvtests.py
@@ -14,15 +14,16 @@ def basic_send_recieve_test(client):
   print "ret = " + repr(ret) + ", old_value = " + repr(old_value)
 
 # assumes a new kvstore
-def set_new_stress_test(client, n, b):
+def set_new_stress_test(client, n, b1, b2):
     fail = False
     start_t = time.time()
     lats = []
-    for i in range(0, n):
+    for i in range(1000, n):
         start_l = time.time()
-        [ret, old_value] = client.kv739_put(str(i), (str(i)*b)[0:128])
+        [ret, old_value] = client.kv739_put((str(i)*b1)[0:128], (str(i)*b2)[0:2048])
         lats.append(time.time()-start_l)
         if int(ret) != 1 or old_value != "":
+            print old_value
             fail = True
     
     print "set_new_stress_test passed:", not fail
@@ -32,15 +33,15 @@ def set_new_stress_test(client, n, b):
            % (min(lats)*1000, (sum(lats) / float(len(lats)))*1000, max(lats)*1000))
 
 # assumes a warm kvstore
-def set_existing_stress_test(client, n, b):
+def set_existing_stress_test(client, n, b1, b2):
     fail = False
     start_t = time.time()
     lats = []
     for i in range(0, n):
         start_l = time.time()
-        [ret, old_value] = client.kv739_put(str(i), (str(i)*b)[0:128])
+        [ret, old_value] = client.kv739_put((str(i)*b1)[0:128], (str(i)*b2)[0:2048])
         lats.append(time.time()-start_l)
-        if int(ret) != 0 or old_value != (str(i)*b)[0:128]:
+        if int(ret) != 0 or old_value != (str(i)*b2)[0:2048]:
             fail = True
     print "set_existing_stress_test passed:", not fail
     print "throughput:", (n / (time.time()-start_t)), "sets/sec"
@@ -50,13 +51,13 @@ def set_existing_stress_test(client, n, b):
 
 
 # assumes keys don't exist
-def get_nonexisting_stress_test(client, n, b):
+def get_nonexisting_stress_test(client, n, b1, b2):
     fail = False
     start_t = time.time()
     lats = []
     for i in range (0, n):
         start_l = time.time()
-        [ret, value] = client.kv739_get(str(i))
+        [ret, value] = client.kv739_get((str(i)*b1)[0:128])
         lats.append(time.time()-start_l)
         if int(ret) != 1 or value != "":
             fail = True
@@ -68,15 +69,15 @@ def get_nonexisting_stress_test(client, n, b):
     
 
 # assumes keys exist
-def get_existing_stress_test(client, n, b):
+def get_existing_stress_test(client, n, b1, b2):
     fail = False
     start_t = time.time()
     lats = []
     for i in range (0, n):
         start_l = time.time()
-        [ret, value] = client.kv739_get(str(i))
+        [ret, value] = client.kv739_get((str(i)*b1)[0:128])
         lats.append(time.time()-start_l)
-        if int(ret) != 0 or value != (str(i)*b)[0:128]:
+        if int(ret) != 0 or value != (str(i)*b2)[0:2048]:
             fail = True
     print "get_existing_stress_test passed:", not fail
     print "throughput:", (n / (time.time()-start_t)), "gets/sec"
@@ -86,16 +87,16 @@ def get_existing_stress_test(client, n, b):
 
 
 # set then get n times test
-def set_getk_stress_test(client, n, k, b):
+def set_getk_stress_test(client, n, k, b1, b2):
     fail = False
     start_t = time.time()
     for i in range(0, n):
-        [ret, old_value] = client.kv739_put(str(i), (str(i)*b)[0:128])
-        if int(ret) != 0 or old_value != (str(i)*b)[0:128]:  
+        [ret, old_value] = client.kv739_put((str(i)*b1)[0:128], (str(i)*b2)[0:2048])
+        if int(ret) != 0 or old_value != (str(i)*b2)[0:2048]:  
             fail = True
         for i in range(0, k):
-            [ret, value] = client.kv739_get(str(i))
-            if int(ret) != 0 or value != (str(i)*b)[0:128]:
+            [ret, value] = client.kv739_get((str(i)*b1)[0:128])
+            if int(ret) != 0 or value != (str(i)*b2)[0:2048]:
                 fail = True
     print "set_getk_stress_test passed:", not fail
     print "throughput:", ((n*k) / (time.time()-start_t)), "set-kgets/sec"
@@ -103,7 +104,7 @@ def set_getk_stress_test(client, n, k, b):
 
 
 if __name__ == "__main__":
-  server_addr = "tcp://adelie-01.cs.wisc.edu:8000"
+  server_addr = "tcp://adelie-03.cs.wisc.edu:8000"
   client = kv739_client()
   if client.kv739_init(server_addr) != 0:
     print "Can't connect to Key-Value store server"
@@ -111,21 +112,23 @@ if __name__ == "__main__":
 
   n = 10000
   k = 5
-  b = 1
-  
+  b1 = 128
+  b2 = 2048
+
   print "Testing Configuration:"
   print "\tn:", n
   print "\tk:", k
-  print "\tb:", b
+  print "\tb1:", b1
+  print "\tb2:", b2
   
   print "------------------------------------------"
-  get_nonexisting_stress_test(client, n, b)
+  get_nonexisting_stress_test(client, n, b1, b2)
   print "------------------------------------------"  
-  set_new_stress_test(client, n, b)
+  set_new_stress_test(client, n, b1, b2)
   print "------------------------------------------"
-  set_existing_stress_test(client, n, b)
+  set_existing_stress_test(client, n, b1, b2)
   print "------------------------------------------"
-  get_existing_stress_test(client, n, b)
+  get_existing_stress_test(client, n, b1, b2)
   print "------------------------------------------"
-  set_getk_stress_test(client, n, k, b)
+  set_getk_stress_test(client, n, k, b1, b2)
   print "------------------------------------------"
