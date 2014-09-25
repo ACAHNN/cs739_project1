44724d98aff5e072d435907b7be14d09357359e4
Aaron Cahn <cahn@optimus.cs.wisc.edu>
2014-09-25 11:12:59 -0500

    basic latency and throughput tests are coded

--- kvtests.py
+++ kvtests.py
@@ -18,47 +18,72 @@ def basic_send_recieve_test(client):
 def set_new_stress_test(client, n, b):
     fail = False
     start_t = time.time()
+    lats = []
     for i in range(0, n):
+        start_l = time.time()
         [ret, old_value] = client.kv739_set(str(i), (str(i)*b)[0:128])
+        lats.append(time.time()-start_l)
         if int(ret) != 1 or old_value != "":
             fail = True
+    
     print "set_new_stress_test passed:", not fail
     print "throughput:", (n / (time.time()-start_t)), "sets/sec"
+    print "latency:"
+    print ("\tmin: %fms, avg: %fms, max: %fms" 
+           % (min(lats)*1000, (sum(lats) / float(len(lats)))*1000, max(lats)*1000))
 
 # assumes a warm kvstore
 def set_existing_stress_test(client, n, b):
     fail = False
     start_t = time.time()
+    lats = []
     for i in range(0, n):
+        start_l = time.time()
         [ret, old_value] = client.kv739_set(str(i), (str(i)*b)[0:128])
+        lats.append(time.time()-start_l)
         if int(ret) != 0 or old_value != (str(i)*b)[0:128]:
             fail = True
     print "set_existing_stress_test passed:", not fail
     print "throughput:", (n / (time.time()-start_t)), "sets/sec"
+    print "latency:"
+    print ("\tmin: %fms, avg: %fms, max: %fms" 
+           % (min(lats)*1000, (sum(lats) / float(len(lats)))*1000, max(lats)*1000))
 
 
 # assumes keys don't exist
 def get_nonexisting_stress_test(client, n, b):
     fail = False
     start_t = time.time()
+    lats = []
     for i in range (0, n):
+        start_l = time.time()
         [ret, value] = client.kv739_get(str(i))
+        lats.append(time.time()-start_l)
         if int(ret) != 1 or value != "":
             fail = True
     print "get_nonexisting_stress_test passed:", not fail
     print "throughput:", (n / (time.time()-start_t)), "gets/sec"
-
+    print "latency:"
+    print ("\tmin: %fms, avg: %fms, max: %fms" 
+           % (min(lats)*1000, (sum(lats) / float(len(lats)))*1000, max(lats)*1000))
+    
 
 # assumes keys exist
 def get_existing_stress_test(client, n, b):
     fail = False
     start_t = time.time()
+    lats = []
     for i in range (0, n):
+        start_l = time.time()
         [ret, value] = client.kv739_get(str(i))
+        lats.append(time.time()-start_l)
         if int(ret) != 0 or value != (str(i)*b)[0:128]:
             fail = True
     print "get_existing_stress_test passed:", not fail
     print "throughput:", (n / (time.time()-start_t)), "gets/sec"
+    print "latency:"
+    print ("\tmin: %fms, avg: %fms, max: %fms" 
+           % (min(lats)*1000, (sum(lats) / float(len(lats)))*1000, max(lats)*1000))
 
 
 # set then get n times test
@@ -87,15 +112,21 @@ if __name__ == "__main__":
 
   n = 1000
   k = 5
-  b = 1
+  b = 128
   
   print "Testing Configuration:"
   print "\tn:", n
   print "\tk:", k
   print "\tb:", b
   
+  print "------------------------------------------"
   get_nonexisting_stress_test(client, n, b)
+  print "------------------------------------------"  
   set_new_stress_test(client, n, b)
+  print "------------------------------------------"
   set_existing_stress_test(client, n, b)
+  print "------------------------------------------"
   get_existing_stress_test(client, n, b)
+  print "------------------------------------------"
   set_getk_stress_test(client, n, k, b)
+  print "------------------------------------------"
